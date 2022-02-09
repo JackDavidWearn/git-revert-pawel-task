89626d8b64e9767572bf868de48399322c95b6e7
Jack Wearn <wearnjack@gmail.com>
2022-02-09 15:49:30 +0000

    Revert "made some changes"
    This reverts commit 4af90e5fb7877a26afeca803e6c5f352208c183c.

--- feature1.java
+++ feature1.java
@@ -1,12 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		for(i = 0; i < 10; i++) {
-			if(i == 5){
-				System.out.println("Have a nice day");
-			} else {
-				System.out.println("Have a bad day!");
-			}
-		}
 	}
 }
